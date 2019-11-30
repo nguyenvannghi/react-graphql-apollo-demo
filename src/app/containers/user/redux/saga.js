d8b018b9128727aec6e571460cc1ce6478a34853
Nghi Nguyen <v.nghinv@vinid.net>
2019-11-30 11:43:09 +0700

    create update user

--- src/app/containers/user/redux/saga.js
+++ src/app/containers/user/redux/saga.js
@@ -1,7 +1,8 @@
-import { fork, put } from 'redux-saga/effects';
+import { fork } from 'redux-saga/effects';
 
+// eslint-disable-next-line require-yield
 function* loginSaga() {
-    return yield put(1);
+    return 1;
 }
 
 export default function* root() {
